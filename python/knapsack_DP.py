c9de1872c72b1786781e4bec3b7517fcb8370708
oe1307 <oe.issa.392@s.kyushu-u.ac.jp>
2022-05-06 19:22:06 +0900

    ナップサック問題をDPで解く

--- python/knapsack_DP.py
+++ python/knapsack_DP.py
@@ -1,4 +1,8 @@
+import re
 from argparse import ArgumentParser
+
+import pandas as pd
+import numpy as np
 
 
 def argparser():
@@ -13,15 +17,32 @@ def argparser():
 
 
 def read_problem(problem):
-    return 0
+    database = list()
+    compiler = re.compile("capacity (?P<capacity>[0-9]+)*")
+    for line in open(problem):
+        if (m := compiler.match(line)) is not None:
+            capacity = int(m.groupdict()["capacity"])
+        else:
+            try:
+                database.append(list(map(int, line.split(" "))))
+            except Exception:
+                pass
+    return capacity, database
 
 
-def main(tmp):
-    pass
+def main(capacity, database):
+    dp = np.zeros((len(database) + 1, capacity + 1))
+    for i, (w, v) in enumerate(database):
+        for cap in range(capacity):
+            if cap + 1 >= w:
+                dp[i + 1][cap + 1] = max(dp[i][cap + 1], dp[i][cap + 1 - w] + v)
+            else:
+                dp[i + 1][cap + 1] = dp[i][cap + 1]
+    print(pd.DataFrame(dp))
 
 
 if __name__ == "__main__":
     parser = argparser()
     args = parser.parse_args()
-    tmp = read_problem(args.problem)
-    main(tmp)
+    capacity, database = read_problem(args.problem)
+    main(capacity, database)
