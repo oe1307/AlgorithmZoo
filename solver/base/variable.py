b54dffc4d235e6bd272ebf38a399954d1757c586
oe1307 <77875118+oe1307@users.noreply.github.com>
2022-06-03 17:10:05 +0900

    Update variable.py

--- solver/base/variable.py
+++ solver/base/variable.py
@@ -19,3 +19,4 @@ class Variable:
         self.value = None
         self.vtype = vtype
         self.name = f"x[{_VARIABLE_NO}]" if name is None else name
+#         参考： https://github.com/flab-coder/flopt/blob/681d2f98824a52bb95de73676823d7ae59c6a013/flopt/variable.py#L366-L521
